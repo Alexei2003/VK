b3124ab265546eae620cb8660c93deb7cf39825b
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2024-05-16 00:55:55 +0300

    instagram F

--- MyCustomClasses/Instagram/InstagramApi.cs
+++ MyCustomClasses/Instagram/InstagramApi.cs
@@ -1,47 +1,13 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace MyCustomClasses.Instagram
 {
     public class InstagramApi
     {
-        private RequestsController requestsController = new RequestsController();
-        private string accessToken;
-        private string userId;
-
-        private InstagramApi() { }
-
-        public InstagramApi(string accessToken, string userId) : this()
+        public static async void Test()
         {
-            Authorize(accessToken, userId);
-        }
-
-        public void Authorize(string accessToken, string userId)
-        {
-            this.accessToken = accessToken;
-            this.userId = userId;
-        }
-
-        public async Task<JObject> CreateMediaObjectAsync(string imageUrl, string caption)
-        {
-            var createMediaData = new
-            {
-                image_url = imageUrl,
-                caption = caption
-            };
-
-            var response = await requestsController.SendPostRequestAsync($"https://graph.instagram.com/v10.0/{userId}/media", createMediaData, accessToken);
-            return await requestsController.ParseResponseAsync(response);
-        }
-
-        public async Task<JObject> PublishMediaAsync(string creationId)
-        {
-            var publishData = new
-            {
-                creation_id = creationId
-            };
-
-            var response = await requestsController.SendPostRequestAsync($"https://graph.instagram.com/v10.0/{userId}/media_publish", publishData, accessToken);
-            return await requestsController.ParseResponseAsync(response);
         }
     }
 }
--- RepetitionOfPostsBot/Program.cs
+++ RepetitionOfPostsBot/Program.cs
@@ -1,8 +1,5 @@
 ﻿using MyCustomClasses;
 using MyCustomClasses.Instagram;
-using MyCustomClasses.VK;
-using Newtonsoft.Json;
-using RepetitionOfPostsBot.BotsTask;
 
 namespace RepetitionOfPostsBot
 {
@@ -13,19 +10,16 @@ namespace RepetitionOfPostsBot
         {
             var accessTokens = GosUslugi.GetAccessTokens();
 
-/*            var vkApi = new VkApiCustom(accessTokens.GetValueOrDefault(GosUslugi.VK));
+            /*            var vkApi = new VkApiCustom(accessTokens.GetValueOrDefault(GosUslugi.VK));
 
-            var threadRepetitionOfPosts = new Thread(new ParameterizedThreadStart(VKTask.RepetitionOfVKPosts));
-            threadRepetitionOfPosts.Start(vkApi);*/
+                        var threadRepetitionOfPosts = new Thread(new ParameterizedThreadStart(VKTask.RepetitionOfVKPosts));
+                        threadRepetitionOfPosts.Start(vkApi);*/
 
-            var instagramApi = new InstagramApi(accessTokens.GetValueOrDefault(GosUslugi.INSTAGRAM), "100065029535432");
-            var a = instagramApi.CreateMediaObjectAsync("https://scontent.cdninstagram.com/v/t51.29350-15/442706645_769424565341139_4575877684158403683_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=VmIBLRDyJLcQ7kNvgHoYMv_&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzM2NzMzMDMxMzcyMDM2MDU5MQ%3D%3D.2-ccb7-5&oh=00_AYBxLce_MG8wa0oaM1DX4eqlKdWOBDb1JBx4qJoyJOf48A&oe=6647B38D&_nc_sid=10d13b", "тест");
-
-            a.Wait();
+            InstagramApi.Test();
 
             while (true)
             {
-                Thread.Sleep(TimeSpan.FromDays(1000000));
+                Thread.Sleep(TimeSpan.FromDays(10));
             }
         }
     }
