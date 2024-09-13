e5753f37bf37b753eddf1310183fce35ddf37d3c
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2024-09-13 21:53:01 +0300

    statics manager add

---
+++ Statistics/Program.cs
@@ -0,0 +1,141 @@
+﻿using ClosedXML.Excel;
+using MyCustomClasses;
+using MyCustomClasses.Tags;
+using MyCustomClasses.Tags.Editors;
+using MyCustomClasses.VK;
+using VkNet.Enums.StringEnums;
+
+internal class Program
+{
+    private const long GROUP_ID = 220199532;
+    private const int SHIFT_POSTS = 100;
+
+    private static void Main(string[] args)
+    {
+        var accessTokens = GosUslugi.GetAccessTokens();
+
+        var api = new VkApiCustom(accessTokens[GosUslugi.VK]);
+
+        var wall = api.Wall.Get(new VkNet.Model.WallGetParams
+        {
+            OwnerId = -1 * GROUP_ID,
+            Count = 100,
+            Filter = WallFilter.All
+        });
+
+        ulong maxOffset = wall.TotalCount;
+        ulong offset = 0;
+
+        var dictTag = new Dictionary<string, TagInfo>();
+
+        while (offset <= maxOffset)
+        {
+            wall = api.Wall.Get(new VkNet.Model.WallGetParams
+            {
+                OwnerId = -1 * GROUP_ID,
+                Count = 100,
+                Offset = offset,
+                Filter = WallFilter.All
+            });
+
+            offset += SHIFT_POSTS;
+
+            foreach (var post in wall.WallPosts)
+            {
+                var postText = post.Text;
+
+                postText = BaseTagsEditor.RemoveBaseTags(postText);
+
+                postText = postText.Replace("!", "");
+                postText = postText.Replace(".", "");
+                postText = postText.Replace("\n", "");
+
+                var tagsArr = postText.Split('#', StringSplitOptions.RemoveEmptyEntries);
+
+                if (tagsArr.Length > 2 || tagsArr.Length == 0 || postText.Contains(' '))
+                {
+                    continue;
+                }
+
+                postText = TagsReplacer.RemoveGroupLinkFromTag(postText);
+
+                var viewsCount = post.Views.Count;
+                var likesCount = post.Likes.Count;
+                var commentsCount = post.Comments.Count;
+                var repostsCount = post.Reposts.Count;
+
+                TagInfo tagInfo;
+                // Сбор инфы настроить
+                try
+                {
+                    tagInfo = dictTag[postText];
+
+                    tagInfo.Count++;
+                    tagInfo.ViewsCount += viewsCount;
+                    tagInfo.LikesCount += likesCount;
+                    tagInfo.CommentsCount += commentsCount;
+                    tagInfo.RepostsCount += repostsCount;
+
+                    dictTag[postText] = tagInfo;
+                }
+                catch
+                {
+                    tagInfo = new TagInfo()
+                    {
+                        Count = 1,
+                        ViewsCount = viewsCount,
+                        LikesCount = likesCount,
+                        CommentsCount = commentsCount,
+                        RepostsCount = repostsCount,
+                    };
+                    dictTag.Add(postText, tagInfo);
+
+                }
+            }
+            Console.WriteLine(offset);
+        }
+
+        using (var workbook = new XLWorkbook())
+        {
+            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+            worksheet.Cell(1, 1).Value = "Название";
+            worksheet.Cell(1, 2).Value = "Количество постов";
+            worksheet.Cell(1, 3).Value = "Просмотры";
+            worksheet.Cell(1, 4).Value = "Лайки";
+            worksheet.Cell(1, 5).Value = "Комментарии";
+            worksheet.Cell(1, 6).Value = "Репосты";
+            worksheet.Cell(1, 7).Value = "Просмотры среднее";
+            worksheet.Cell(1, 8).Value = "Лайки среднее";
+            worksheet.Cell(1, 9).Value = "Комментарии среднее";
+            worksheet.Cell(1, 10).Value = "Репосты среднее";
+
+            int i = 2;
+            foreach (var tag in dictTag)
+            {
+                worksheet.Cell(i, 1).Value = tag.Key;
+                worksheet.Cell(i, 2).Value = tag.Value.Count;
+                worksheet.Cell(i, 3).Value = tag.Value.ViewsCount;
+                worksheet.Cell(i, 4).Value = tag.Value.LikesCount;
+                worksheet.Cell(i, 5).Value = tag.Value.CommentsCount;
+                worksheet.Cell(i, 6).Value = tag.Value.RepostsCount;
+                worksheet.Cell(i, 7).Value = tag.Value.ViewsCount / tag.Value.Count;
+                worksheet.Cell(i, 8).Value = tag.Value.LikesCount / tag.Value.Count;
+                worksheet.Cell(i, 9).Value = tag.Value.CommentsCount / tag.Value.Count;
+                worksheet.Cell(i, 10).Value = tag.Value.RepostsCount / tag.Value.Count;
+
+                i++;
+            }
+            workbook.SaveAs("tags.xlsx");
+        }
+    }
+
+    private struct TagInfo
+    {
+        public int Count { get; set; }
+        public int ViewsCount { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+        public int RepostsCount { get; set; }
+    }
+}
