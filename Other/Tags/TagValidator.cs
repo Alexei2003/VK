004391309fdb8ed21a4e0d050854bdc4cc6e5e5e
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2026-03-13 01:48:45 +0300

    fix

--- Other/Tags/TagValidator.cs
+++ Other/Tags/TagValidator.cs
@@ -2,7 +2,7 @@
 {
     public static class TagValidator
     {
-        private static readonly string[] _badTagArr = { "bad_drawing", "nsfw" };
+        private static readonly string[] _badTagArr = { "#bad_drawing", "#nsfw" };
 
         public static bool CheckBadTag(string[] tagsArr)
         {
